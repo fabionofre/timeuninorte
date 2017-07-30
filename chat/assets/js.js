3b7fb71d2eaf45dc9f94421f16a9bcb2a400b8ae
Fábio Onofre <fabio.onofre166@gmail.com>
2017-07-29 23:41:27 -0500

    Segundo commit report-status da noite 29

--- chat/assets/js.js
+++ chat/assets/js.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var socket = io.connect("http://localhost:3000");
+    var socket = io.connect("http://172.16.60.223:4000");
     var ready = false;
 
     $("#submit").submit(function(e) {
